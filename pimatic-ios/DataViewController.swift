68f11492ed10e57c590c075a269978a5b2483d55
Moritz Weichert <moritz.weichert@darktek.de>
2016-06-27 14:39:17 +0200

    Initial Commit

---
+++ pimatic-ios/DataViewController.swift
@@ -0,0 +1,34 @@
+//
+//  DataViewController.swift
+//  pimatic-ios
+//
+//  Created by Moritz Weichert on 27.06.16.
+//  Copyright © 2016 Moritz Weichert. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
